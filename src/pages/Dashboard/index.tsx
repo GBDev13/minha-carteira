0bcfc2d1270b11e44616b6ccde9e499b9b839b03
Gabriel Borges <gbdesigns13@gmail.com>
2021-03-21 16:44:19 -0300

    Saved

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,9 @@
+const Dashboard: React.FC = () => {
+  return (
+    <h1>
+      Dashboard
+    </h1>
+  )
+}
+
+export default Dashboard
---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,9 @@
+const SignIn: React.FC = () => {
+  return (
+    <h1>
+      SignIn
+    </h1>
+  )
+}
+
+export default SignIn
